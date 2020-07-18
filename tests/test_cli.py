12682c30b839d3196e54b692a5d15f382c787810
Stefan Hagen <stefan@hagen.link>
2020-07-18 22:05:35 +0200

    Added type correct but dimension not test on cli/main that exposes surprising feature/bug

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,6 +5,11 @@ import pytest  # type: ignore
 import prefix_compression.cli as cli
 
 
+def test_main_nok_string():
+    with pytest.raises(TypeError):
+        cli.main("dimension is wrong")
+
+
 def test_main_nok_ints():
     with pytest.raises(TypeError):
         cli.main([1, 2, 3])
