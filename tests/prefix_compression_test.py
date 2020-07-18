d46e017ccc5bfa8404e41a2e490d2188d1b8ef82
Stefan Hagen <stefan@hagen.link>
2020-07-18 19:46:15 +0200

    Added test for empty sequence

--- tests/prefix_compression_test.py
+++ tests/prefix_compression_test.py
@@ -13,6 +13,10 @@ def test_prefix_compression_ok_strings():
     assert pc.prefix_compression(["aa", "ab"]) == ('a', ['a', 'b'])
 
 
+def test_prefix_compression_ok_empty():
+    assert pc.prefix_compression([]) == ('', [])
+
+
 def test_prefix_compression_nok_ints():
     with pytest.raises(TypeError):
         pc.prefix_compression([1, 2, 3])
@@ -20,11 +24,11 @@ def test_prefix_compression_nok_ints():
 
 def test_documentation_example():
     sequence = ('bar/baz', 'bar/bazaar')
-    expect = ('bar/', ('baz', 'bazaar'))
+    expect = ('bar/', ['baz', 'bazaar'])
     assert pc.prefix_compression(sequence, policy=lambda x: x == '/') == expect
 
 
 def test_documentation_no_policy_example():
     sequence = ('bar/baz', 'bar/bazaar')
-    expect = ('bar/baz', ('', 'aar'))
+    expect = ('bar/baz', ['', 'aar'])
     assert pc.prefix_compression(sequence, policy=None) == expect
