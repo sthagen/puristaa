6fa09e9acf7e79c40ede6b92ff209ec3fddef176
Stefan Hagen <stefan@hagen.link>
2020-07-18 20:06:17 +0200

    Started black formating the tests

--- tests/prefix_compression_test.py
+++ tests/prefix_compression_test.py
@@ -12,19 +12,19 @@ def test_main_nok_ints():
 
 
 def test_prefix_compression_ok_string():
-    assert pc.prefix_compression(["aa"]) == ('aa', [''])
+    assert pc.prefix_compression(["aa"]) == ("aa", [""])
 
 
 def test_prefix_compression_ok_strings():
-    assert pc.prefix_compression(["aa", "ab"]) == ('a', ['a', 'b'])
+    assert pc.prefix_compression(["aa", "ab"]) == ("a", ["a", "b"])
 
 
 def test_prefix_compression_ok_disjoint_strings():
-    assert pc.prefix_compression(["a", "b"]) == ('', ['a', 'b'])
+    assert pc.prefix_compression(["a", "b"]) == ("", ["a", "b"])
 
 
 def test_prefix_compression_ok_empty():
-    assert pc.prefix_compression([]) == ('', [])
+    assert pc.prefix_compression([]) == ("", [])
 
 
 def test_prefix_compression_nok_ints():
@@ -34,16 +34,16 @@ def test_prefix_compression_nok_ints():
 
 def test_prefix_compression_nok_floats():
     with pytest.raises(TypeError):
-        pc.prefix_compression([.123, 3.1415])
+        pc.prefix_compression([0.123, 3.1415])
 
 
 def test_documentation_example():
-    sequence = ['bar/baz', 'bar/bazaar']
-    expect = ('bar/', ['baz', 'bazaar'])
-    assert pc.prefix_compression(sequence, policy=lambda x: x == '/') == expect
+    sequence = ["bar/baz", "bar/bazaar"]
+    expect = ("bar/", ["baz", "bazaar"])
+    assert pc.prefix_compression(sequence, policy=lambda x: x == "/") == expect
 
 
 def test_documentation_no_policy_example():
-    sequence = ['bar/baz', 'bar/bazaar']
-    expect = ('bar/baz', ['', 'aar'])
+    sequence = ["bar/baz", "bar/bazaar"]
+    expect = ("bar/baz", ["", "aar"])
     assert pc.prefix_compression(sequence, policy=None) == expect
