590fa3fb2d374dc8e9f73c94042d9f563f382f32
Stefan Hagen <stefan@hagen.link>
2020-07-18 22:01:36 +0200

    Created dedicated cli test module

---
+++ tests/test_cli.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=missing-docstring,unused-import,reimported
+import pytest  # type: ignore
+
+import prefix_compression.cli as cli
+
+
+def test_main_nok_ints():
+    with pytest.raises(TypeError):
+        cli.main([1, 2, 3])
+
