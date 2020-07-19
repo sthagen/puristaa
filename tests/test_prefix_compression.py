21c57443d7b92e09071b2940bafae74c2ae7f533
Stefan Hagen <stefan@hagen.link>
2020-07-19 15:24:10 +0200

    Added test for receiving a dict and the (technically) correct compression

--- tests/test_prefix_compression.py
+++ tests/test_prefix_compression.py
@@ -65,3 +65,9 @@ def test_documentation_nok_set_no_policy_example():
     sequence = {"bar/baz", "bar/bazaar"}
     with pytest.raises(TypeError):
         pc.prefix_compression(sequence, policy=None)
+
+
+def test_documentation_ok_dict_no_policy_example():
+    mapping = {0: "bar/baz", 1: "bar/bazaar"}
+    expect = ('', [{0: 'bar/baz', 1: 'bar/bazaar'}])
+    assert pc.prefix_compression(mapping, policy=None) == expect
