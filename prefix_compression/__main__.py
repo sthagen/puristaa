80f63d504b38afc642ecdf481b1d37307be5610a
Stefan Hagen <stefan@hagen.link>
2020-12-25 19:34:43 +0100

    Moved no cover pragma to correct line.

--- prefix_compression/__main__.py
+++ prefix_compression/__main__.py
@@ -4,5 +4,5 @@ import sys
 
 from prefix_compression.cli import main
 
-if __name__ == "__main__":  # pragma: no cover
-    sys.exit(main(sys.argv[1:]))
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))  # pragma: no cover
